11c677778d05e323a9632269b721f903eab44494
Thomas Hansen <lyngtun@gmail.com>
2022-12-10 10:40:58 +0100

    Code 10 - part 1 done

--- 2022/10/Program.cs
+++ 2022/10/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var lines = System.IO.File.ReadLines(@"input/input.txt");
+
+var currentCycle = 1;
+var checkCycle = 20;
+const int checkInterval = 40;
+var checkVals = new List<int>();
+var regX = 1;
+
+foreach(var line in lines) {
+    var cmd = line.Split(" ");
+    if(cmd[0] == "noop") {
+        if(currentCycle == checkCycle) {
+            checkVals.Add(regX * checkCycle);
+            Console.WriteLine($"Cycle {currentCycle}, value {regX}, add val {regX*checkCycle}");
+            checkCycle += checkInterval;
+        }
+        currentCycle++;
+    }
+    else if(cmd[0] == "addx") {
+        var val = Int32.Parse(cmd[1]);
+
+        if(currentCycle == checkCycle || currentCycle+1 == checkCycle) {
+            checkVals.Add(regX * checkCycle);
+            Console.WriteLine($"Cycle {currentCycle}, value {regX}, add val {regX*checkCycle}");
+            checkCycle += checkInterval;
+        }
+        currentCycle += 2;
+        regX += val;
+    }
+}
+
+var sum = checkVals.Sum();
+Console.WriteLine($"Sum of check cycle products: {sum}");

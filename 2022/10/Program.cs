dca2e63d9b77efdc6bf68d48885441deb27307e7
Thomas Hansen <lyngtun@gmail.com>
2022-12-10 11:20:35 +0100

    Code 10 - part 2 done

--- 2022/10/Program.cs
+++ 2022/10/Program.cs
@@ -1,10 +1,22 @@
-﻿var lines = System.IO.File.ReadLines(@"input/input.txt");
+﻿var lines = System.IO.File.ReadLines(@"input/input.txt").ToArray();
 
 var currentCycle = 1;
 var checkCycle = 20;
 const int checkInterval = 40;
 var checkVals = new List<int>();
 var regX = 1;
+
+List<char> display = new List<char>();
+
+void computeCycle(int cycle, int spritePos) {
+    var colNo = (cycle-1) % 40;
+    if(colNo >= spritePos-1 && colNo <= spritePos+1) {
+        display.Add('#');
+    }
+    else {
+        display.Add('.');
+    }
+}
 
 foreach(var line in lines) {
     var cmd = line.Split(" ");
@@ -14,6 +26,8 @@ foreach(var line in lines) {
             Console.WriteLine($"Cycle {currentCycle}, value {regX}, add val {regX*checkCycle}");
             checkCycle += checkInterval;
         }
+
+        computeCycle(currentCycle, regX);
         currentCycle++;
     }
     else if(cmd[0] == "addx") {
@@ -24,6 +38,9 @@ foreach(var line in lines) {
             Console.WriteLine($"Cycle {currentCycle}, value {regX}, add val {regX*checkCycle}");
             checkCycle += checkInterval;
         }
+
+        computeCycle(currentCycle, regX);
+        computeCycle(currentCycle+1, regX);
         currentCycle += 2;
         regX += val;
     }
@@ -31,3 +48,17 @@ foreach(var line in lines) {
 
 var sum = checkVals.Sum();
 Console.WriteLine($"Sum of check cycle products: {sum}");
+
+var line1 = new string(display.GetRange(0, 40).ToArray());
+var line2 = new string(display.GetRange(40, 40).ToArray());
+var line3 = new string(display.GetRange(80, 40).ToArray());
+var line4 = new string(display.GetRange(120, 40).ToArray());
+var line5 = new string(display.GetRange(160, 40).ToArray());
+var line6 = new string(display.GetRange(200, 40).ToArray());
+
+Console.WriteLine(line1);
+Console.WriteLine(line2);
+Console.WriteLine(line3);
+Console.WriteLine(line4);
+Console.WriteLine(line5);
+Console.WriteLine(line6);
